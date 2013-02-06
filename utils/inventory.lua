b39b68c181a257db4b3a669d97c766dc6a375703
Sami Saada <saada@cs.helsinki.fi>
2013-02-06 23:54:22 +0200

    namespace inventory

--- utils/inventory.lua
+++ utils/inventory.lua
@@ -1,11 +1,11 @@
 local _G = getfenv(0)
 local print, tostring = _G.print, _G.tostring
 
-function PrintInventory(inventory)
+local function PrintInventory(inventory)
   if not inventory then
     return
   end
-  for i = 1, 12, 1  do
+  for i = 1, #inventory, 1  do
     local curItem = inventory[i]
     if curItem then
       print(tostring(i)..', '..curItem:GetName()..'\n')
@@ -15,13 +15,21 @@ function PrintInventory(inventory)
   end
 end
 
-function HasItemsInInventory(unit)
+local function HasItemsInInventory(unit)
   local inventory = unit:GetInventory()
   return #inventory > 0
 end
 
-function HasItemsInStash(unit)
+local function HasItemsInStash(unit)
   local inventory_with_stash = unit:GetInventory(true)
   local inventory = unit:GetInventory()
   return #inventory_with_stash - #inventory > 0
+end
+
+function Inventory()
+  local functions = {}
+  functions.PrintInventory = PrintInventory
+  functions.HasItemsInInventory = HasItemsInInventory
+  functions.HasItemsInStash = HasItemsInStash
+  return functions
 end
