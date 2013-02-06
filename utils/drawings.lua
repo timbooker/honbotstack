3f75ccdea349a2ab461bd23ebd494db9d3d64275
Sami Saada <saada@cs.helsinki.fi>
2013-02-06 17:02:31 +0200

    refactor drawing

--- utils/drawings.lua
+++ utils/drawings.lua
@@ -1,15 +1,19 @@
 local _G = getfenv(0)
 
-function DrawXPosition(position, color, size)
-  if not position then
-    return
-  end
+local defaultColor = "red"
 
-  color = color or "red"
-  size = size or 100
+function DrawLine(startPosition, endPosition, color)
+  if not startPosition or not endPosition then return end
+  HoN.DrawDebugLine(startPosition, endPosition, false, color or defaultColor)
+end
+
+function DrawXPosition(position, color, size)
+  if not position then return end
+
+  size = size or 50
   local tl = Vector3.Create(0.5, -0.5) * size
   local bl = Vector3.Create(0.5, 0.5) * size
 
-  HoN.DrawDebugLine(position - 0.5 * tl, position + 0.5 * tl, false, color)
-  HoN.DrawDebugLine(position - 0.5 * bl, position + 0.5 * bl, false, color)
+  DrawLine(position - tl, position + tl, color)
+  DrawLine(position - bl, position + bl, color)
 end
