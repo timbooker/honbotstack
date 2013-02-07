ca1ab90d2c111c49fb91da713f85078427de0a74
Sami Saada <saada@cs.helsinki.fi>
2013-02-07 18:58:25 +0200

    use abilities of flying courier

--- utils/courier_deliver.lua
+++ utils/courier_deliver.lua
@@ -1,5 +1,8 @@
 local _G = getfenv(0)
 
+local courierShieldRange = 1000
+local courierShieldUnitMasks = 0x0000020 + 0x0000001
+
 local function IsNearStash(courier)
   return courier and courier:CanAccessStash()
 end
@@ -34,9 +37,17 @@ local function MoveItemsToCourier(hero, courier)
   end
 end
 
+local function IsFlying(courier)
+  return courier:GetTypeName() == 'Pet_FlyngCourier'
+end
+
 local function DeliverItems(bot, courier)
   local deliver = courier:GetAbility(2)
   bot:OrderAbility(deliver)
+  if IsFlying(courier) then
+    local speedBurst = courier:GetAbility(0)
+    bot:OrderAbility(speedBurst)
+  end
 end
 
 local function ReturnToPool(bot, courier)
@@ -60,7 +71,30 @@ local function HasDelivered(bot, courier)
     data.returning = true
   end
 
-  if data.delivering then
+  local function protect(bot, courier)
+    local shield = courier:GetAbility(1)
+    bot:OrderAbility(shield)
+  end
+
+  local function enemiesNear(bot, courier)
+    local units = HoN.GetUnitsInRadius(courier:GetPosition(), courierShieldRange, courierShieldUnitMasks, true)
+
+    for key, unit in pairs(units) do
+      if not (unit:GetTeam() == bot:GetTeam()) and unit:GetCanAttack() then
+        return true
+      end
+    end
+    return false
+  end
+
+  local function canActiveProtect(courier)
+    local shield = courier:GetAbility(1)
+    return shield:CanActivate()
+  end
+
+  if IsFlying(courier) and enemiesNear(bot, courier) and canActiveProtect(courier) then
+    protect(bot, courier)
+  elseif data.delivering then
     if bot:IsDead() then
       toPool()
     elseif not CourierHasItems(courier) then
